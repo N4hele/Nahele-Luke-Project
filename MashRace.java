1186cb2904a40f39e80f89a95cc559a805202eca
Your Name <you@example.com>
2022-12-09 15:05:01 -0800

    eeeeeee Co-authored-by: Spicytuna24 <Spicytuna24@users.noreply.github.com>

--- MashRace.java
+++ MashRace.java
@@ -1,9 +1,11 @@
-    import java.awt.*;
-    import javax.swing.*;
-    import java.util.Random;
-    import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-    public class MashRace extends JPanel {
+public class MashRace extends JPanel {
 
     private JFrame myFrame;
     private Random randGen = new Random();
@@ -20,20 +22,24 @@
     int topBorder = 10; // allow for the menu bar at the top of the frame
     int border = 10; // the border around the dots
     int sideBorder = startLine; // space after the finishLine
-    int windowWidth = 500;
-    int windowHeight= 500;
+    int windowWidth = 600;
+    int windowHeight= 1000;
 
-    int racer1X = 200;
-    int racer1Y = 400;
-    int racer2X = 300;
-    int racer2Y = 400;
+ 
+    int racerY = 900;
+
+
+
+
 
 
     // This ArrayList holds the Dot objects in the race
     ArrayList<Racer> racers = new ArrayList<Racer>();
-
     // constructor
     public MashRace() {
+        KeyListener listener = new MyKeyListener();
+		addKeyListener(listener);
+		setFocusable(true);
         initRacers();
         myFrame = new JFrame("Mash Racers");
         myFrame.add(this);
@@ -43,34 +49,82 @@
     }
 
     public void initRacers(){
-        racers.add(new Racer("Racer1", 200));
-        racers.add(new Racer("Racer2", 300));
+        racers.add(new Racer("Racer 1", 130));
+        racers.add(new Racer("Racer 2", 400));
     }
 
     public void runRace(){
-        while(true){
+        while(endgame == false){
             
         }
     }
 
     public void paintComponent(Graphics g){
         super.repaint();
+        g.drawLine(-100, 101, 700 , 101);
+        g.drawLine(-100, 102, 700 , 102);
+        g.drawLine(-100, 103, 700 , 103);
+        g.drawLine(-100, 104, 700 , 104);
+
         for(Racer r:racers){
             drawRacer(r, g);
         }
         
     }
 
-    
+    public class MyKeyListener implements KeyListener{
+        @Override
+		public void keyTyped(KeyEvent e) {
+		}
 
+		@Override
+		public void keyPressed(KeyEvent e) {
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+            if(KeyEvent.getKeyText(e.getKeyCode()).equals("A") && endgame == false){
+                racers.get(0).moved(-10);
+            }
+            if(KeyEvent.getKeyText(e.getKeyCode()).equals("L") && endgame == false){
+                racers.get(1).moved(-10);
+		    }
+        }
+    }
+
+    private boolean endgame;
     public void drawRacer(Racer racer, Graphics g) {
         int topX = racer.getStartX();
-        int topY = racer1Y + racer.getDistance();
+        int topY = racerY + racer.getDistance();
         int drawDiam = 50;
+        if(topY<100){
+            g.setFont(new Font("Ariel", Font.BOLD, 72));
+            g.drawString(racer.getName() + " WINS", 65, 97);
+            System.out.println(racer.getName() + " WINS");
+            endgame = true;
+        }
+
+        g.setColor(Color.green);
         g.fillOval(topX, topY, drawDiam, drawDiam);
         g.setColor(Color.black);
         g.drawOval(topX, topY, drawDiam, drawDiam);
+
+       
+        g.setColor(Color.white);
+        g.fillOval(topX, topY, drawDiam-30, drawDiam-30);
+        g.fillOval(topX+30, topY, drawDiam-30, drawDiam-30);
+    
+        g.setColor(Color.black);
+        g.drawOval(topX+30, topY, drawDiam-30, drawDiam-30);
+        g.drawOval(topX, topY, drawDiam-30, drawDiam-30);
+
+        g.fillOval(topX+5, topY, drawDiam-40, drawDiam-40);
+        g.fillOval(topX+35, topY, drawDiam-40, drawDiam-40);
+
+        
+
     }
 
 
+    
 }
